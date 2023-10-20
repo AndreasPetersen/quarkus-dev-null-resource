46e4a2b1fe5fa8a5a1febc59a024b0ef543707d7
Andreas Petersen <andrpetersen@gmail.com>
2023-10-20 06:55:49 +0000

    Reproducer

---
+++ my-app/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,29 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        SwaggerParseResult swaggerParseResult = new OpenAPIV3Parser().readLocation("openapi.json", null, null);
+        if (!swaggerParseResult.getMessages().isEmpty()) {
+            throw new IllegalArgumentException(String.join(", ", swaggerParseResult.getMessages()));
+        }
+
+        return "Hello OpenAPI";
+    }
+}
